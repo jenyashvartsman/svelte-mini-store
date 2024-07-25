61a023c7638f1a0258e6972828cb113ab0b5ae84
Jenya Shvartsman <jenyas@1touch.io>
2024-07-25 15:17:07 -0400

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  import "./app.css";
+</script>
+
+<main>app</main>
+
+<style>
+</style>
---
+++ src/lib/dto/product.dto.ts
@@ -0,0 +1,6 @@
+export interface ProductDto {
+  name: string;
+  price: number;
+  category: string;
+  img: string;
+}
